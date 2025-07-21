5e08e218b6066c04fb2548b62dec168b93e81150
diwanemonika <diwanemonika@gmail.com>
2025-07-21 12:54:22 +0530

    Add .next/static/chunks/src_app_insuregram_page_tsx_0ef0cac3._.js 1753082661585

---
+++ .next/static/chunks/src_app_insuregram_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_70bf0aae._.js",
+  "static/chunks/node_modules_49b39bcf._.js"
+],
+    source: "dynamic"
+});
