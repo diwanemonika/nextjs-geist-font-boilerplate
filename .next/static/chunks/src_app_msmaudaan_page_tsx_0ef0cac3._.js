e9dd41c921db7514bbc84fe4ca3390a7fb1af309
diwanemonika <diwanemonika@gmail.com>
2025-07-21 12:54:05 +0530

    Add .next/static/chunks/src_app_msmaudaan_page_tsx_0ef0cac3._.js 1753082644759

---
+++ .next/static/chunks/src_app_msmaudaan_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_5431a65c._.js",
+  "static/chunks/node_modules_e40313dd._.js"
+],
+    source: "dynamic"
+});
